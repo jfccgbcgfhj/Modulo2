2c6a7bb0bbb93fc8044bc3e91e71906332783d00
jfccgbcgfhj <32169169+jfccgbcgfhj@users.noreply.github.com>
2017-11-24 15:13:16 +0000

    Add files via upload

---
+++ hojesexta.java
@@ -0,0 +1,14 @@
+
+package javaapplication1;
+
+public class hojesexta {
+
+
+	public static void main(String[] args) {
+		String mensagem = "Hoje é Sexta-Feira!";
+		for (int I=1; I<=5; I++)
+			System.out.println(mensagem);
+
+	}
+
+}
