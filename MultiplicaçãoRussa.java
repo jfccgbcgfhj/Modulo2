d995634444ffff66d0c9670efe7a4a1c8d122c00
jfccgbcgfhj <32169169+jfccgbcgfhj@users.noreply.github.com>
2017-12-15 12:05:43 +0000

    Add files via upload

---
+++ MultiplicaçãoRussa.java
@@ -0,0 +1,20 @@
+package animais;
+
+import java.util.Scanner;
+
+public class MultiplicaçãoRussa {
+    public static void main(String[] args) {
+  int X, Y;
+  Scanner ler = new Scanner (System.in);
+  System.out.print("Indique o multiplicando");
+  X= ler.nextInt();
+  System.out.print("Qual é o multiplicador");
+  Y= ler.nextInt();
+  System.out.print("O multiplicando é" + X + "/n");
+  System.out.print("O multiplicador é" + Y);
+  
+  
+  
+    }
+    
+}
